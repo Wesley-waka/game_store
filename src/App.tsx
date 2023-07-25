14eddc5bb87147db987a4a873ff1f4da6bc4142f
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-25 12:07:27 +0300

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" main"`,
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral"></GridItem>
+      <Show above="lg"></Show>
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
