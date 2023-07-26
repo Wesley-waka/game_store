67249daefb380620dc0996c9d8c53709a9d4b0aa
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-26 15:16:20 +0300

    add a dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,22 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+const GameHeading = ({ gameQuery }: Props) => {
+  // Games
+  // Xbox
+  // PS
+  const heading = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+  return (
+    <Heading as="h1" marginY={5} fontSize="5xl">
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
