ae2da746d34553ff7abf8cbca4871ceb887027d3
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-26 09:39:11 +0300

    fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,36 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {}
+const useGenres = () => {
+  const [genres, setGenres] = useState<Game[]>([]);
+  const [error, setError] = useState(" ");
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGamesResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+    //eslint disable next line
+  }, []);
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
