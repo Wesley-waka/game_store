9567500373ba26692f8a78b5e6feb64f352427b7
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-07-26 13:05:01 +0300

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text />
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
