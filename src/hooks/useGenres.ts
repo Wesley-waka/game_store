9915843d6f645c8179170af1a6f7e6f85d5abc1e
Wesley-waka <wesleywaka77@gmail.com>
2023-08-01 10:21:38 +0300

    API Read:get genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import genres from "../data/genres";
 
 export interface Genre {
@@ -6,6 +9,23 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "e53bba19d7914970889f528d70c8e06d",
+  },
+});
+
+const getGenres = () => {
+  return axiosInstance.get<Genre>("genres").then((res) => res.data);
+};
+
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () => getGenres,
+    staleTime: ms("24h"),
+    initialData: genres,
+  });
 
 export default useGenres;
